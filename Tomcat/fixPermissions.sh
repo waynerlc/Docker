6d14634d0f1b9104d42b102f08e49d9eac323c74
waynerlc <waynerlc@gmail.com>
2014-12-04 17:12:21 -0200

    Novos scripts

---
+++ Tomcat/fixPermissions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo chown -R bematech:bematech /NFCE/NEW
+sudo chown -R bematech:bematech /NFCE/app
+sudo chown -R bematech:bematech /NFCE/BACKUP
+sudo chown -R bematech:bematech /NFCE/ERRORS
+sudo chown -R bematech:bematech /NFCE/SIGN
+sudo chown -R bematech:bematech /NFCE/CUSTODIA
--- Tomcat/run.sh
+++ Tomcat/run.sh
@@ -2,6 +2,6 @@
 
 docker rm tomcat
 echo Inicializando o container do tomcat7...
-docker run --name tomcat -u bematech -d -v /NFCE/tomcat-conf:/home/bematech/tomcat-conf -p 8080:8080 bema/tomcat
+docker run --name tomcat -u bematech -d -v /NFCE/tomcat-conf:/home/bematech/tomcat-conf --volumes-from data -p 8080:8080 bema/tomcat
 echo Fim.
 
